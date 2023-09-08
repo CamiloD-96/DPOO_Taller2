10378d2f9416a146cbadbf0cef8807af09cfece7
Camilo Daza Portatil <camilodaza-96@hotmail.com>
2023-09-07 23:48:05 -0500

    Commit productoAjustado

---
+++ src/uniandes/dpoo/taller2/modelo/ProductoAjustado.java
@@ -0,0 +1,27 @@
+package uniandes.dpoo.taller2.modelo;
+
+public class ProductoAjustado implements Interface {
+
+	public ProductoAjustado(ProductoMenu base)
+	{
+		
+	}
+	@Override
+	public double darPrecio() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String darNombre() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String generarTextoFactura() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
