94cb754a0b7bcc1a6f576c46c7750d82a91c3d05
Camilo Daza Portatil <camilodaza-96@hotmail.com>
2023-09-20 14:20:42 -0500

    Avance general importante

--- src/uniandes/dpoo/taller2/consola/Consola.java
+++ src/uniandes/dpoo/taller2/consola/Consola.java
@@ -3,16 +3,17 @@ package uniandes.dpoo.taller2.consola;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Collection;
+import java.io.InputStreamReader;
+import java.util.List;
 
-import uniandes.dpoo.taller0.procesamiento.LoaderOlimpicos;
+import uniandes.dpoo.taller2.modelo.Combo;
+import uniandes.dpoo.taller2.modelo.Ingrediente;
+import uniandes.dpoo.taller2.modelo.ProductoMenu;
 import uniandes.dpoo.taller2.procesamiento.ControllerRestaurante;
-import uniandes.dpoo.taller2.modelo.Restaurante;
+import uniandes.dpoo.taller2.procesamiento.LoaderRestaurante;
 public class Consola {
 	
-	private Restaurante restaurante;
-	
-	private String archivo;
+	private ControllerRestaurante controllerRestaurante;
 
 
 	/**
@@ -22,49 +23,32 @@ public class Consola {
 	 */
 	public void ejecutarAplicacion()
 	{
-		System.out.println("Bienvenido a su Restaurante preferido!, El Norral! \n");
+		System.out.println("\n\nBienvenido a su Restaurante preferido!, El Norral! \n");
 
 		boolean continuar = true;
 		while (continuar)
 		{
 			try
 			{
-				mostrarMenu();
+				mostrarOpciones();
 				int opcion_seleccionada = Integer.parseInt(input("Por favor seleccione una opción"));
+				if (opcion_seleccionada == 0)
+					mostrarMenu();
+					mostrarOpciones();
 				if (opcion_seleccionada == 1)
-					ejecutarCargarAtletas();
-				else if (opcion_seleccionada == 2 && calculadora != null)
-					ejecutarAtletasPorAnio();
-				else if (opcion_seleccionada == 3 && calculadora != null)
-					ejecutarMedallasEnRango();
-				else if (opcion_seleccionada == 4 && calculadora != null)
-					ejecutarAtletasPorPais();
-				else if (opcion_seleccionada == 5 && calculadora != null)
-					ejecutarPaisConMasMedallistas();
-				else if (opcion_seleccionada == 6 && calculadora != null)
-					ejecutarMedallistasPorEvento();
-				else if (opcion_seleccionada == 7 && calculadora != null)
-					ejecutarAtletasConMasMedallasQue();
-				else if (opcion_seleccionada == 8 && calculadora != null)
-					ejecutarAtletaEstrella();
-				else if (opcion_seleccionada == 9 && calculadora != null)
-					ejecutarMejorPaisEnUnEvento();
-				else if (opcion_seleccionada == 10 && calculadora != null)
-					ejecutarTodoterreno();
-				else if (opcion_seleccionada == 11 && calculadora != null)
-					ejecutarMedallistasPorNacionYGenero();
-				else if (opcion_seleccionada == 12 && calculadora != null)
-					ejecutarPorcentajeMedallistas();
-				else if (opcion_seleccionada == 13 && calculadora != null)
-					ejecutarPaisAtlteta();
-				else if (opcion_seleccionada == 14)
+					iniciarPedido();
+				else if (opcion_seleccionada == 2)
+					cerrarYGuardarPedido();
+				else if (opcion_seleccionada == 3)
+					agregarIngredientePedido();
+				else if (opcion_seleccionada == 4)
+					eliminarIngredientePedido();
+				else if (opcion_seleccionada == 5)
+					consultarPedido();
+				else if (opcion_seleccionada == 6)
 				{
 					System.out.println("Saliendo de la aplicación ...");
 					continuar = false;
-				}
-				else if (calculadora == null)
-				{
-					System.out.println("Para poder ejecutar esta opción primero debe cargar un archivo de atletas.");
 				}
 				else
 				{
@@ -78,47 +62,109 @@ public class Consola {
 		}
 	}
 
+
+
+	private void iniciarPedido() {
+		// TODO Auto-generated method stub
+		System.out.println("Para iniciar su pedido digita la siguiente información: \n\n");
+		String nombre = input("Digite su nombre: ");
+		String direccion = input("Digite su dirección: ");
+		controllerRestaurante.iniciarPedido(nombre, direccion);
+		System.out.println("\nPedido registrado");
+	}
+	
+	private void cerrarYGuardarPedido() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private void consultarPedido() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void eliminarIngredientePedido() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private void agregarIngredientePedido() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
 	/**
 	 * Muestra al usuario el menú con las opciones para que escoja la siguiente
 	 * acción que quiere ejecutar.
 	 */
-	public void mostrarMenu()
+	public void mostrarOpciones()
 	{
 		System.out.println("\nOpciones de la aplicación\n");
+		System.out.println("0. Mostrar Menú");
 		System.out.println("1. Iniciar pedido");
 		System.out.println("2. Cerrar y guardar pedido");
-		System.out.println("3. Consultar las medallas de un atleta en un periodo");
-		System.out.println("4. Consultar los atletas de un país dado");
-		System.out.println("5. Consultar el país con más medallistas");
-		System.out.println("6. Consultar todos los medallistas de un evento dado");
-		System.out.println("7. Consultar los atletas con un mínimo de medallas");
-		System.out.println("8. Consultar el atleta estrella de todos los tiempos");
-		System.out.println("9. Consultar mejor país en un evento");
-		System.out.println("10. Consultar el atleta todoterreno");
-		System.out.println("11. Consultar los medallistas por país y género");
-		System.out.println("12. Consultar el porcentaje de atletas que son medallistas");
-		System.out.println("13. Consultar el país de un atleta");
-		System.out.println("14. Salir de la aplicación\n");
+		System.out.println("3. Agregar ingrediente al pedido");
+		System.out.println("4. Eliminar ingrediente al pedido");
+		System.out.println("5. Consultar sobre un pedido, ingresando el id");
+		System.out.println("6. Salir de la aplicación\n");
+	}
+	
+	
+	public void mostrarMenu()
+	{
+		System.out.println("\n---- MENÚ ----\n");
 		
+		// Productos
+        System.out.println("Productos:\n");
+        List<ProductoMenu> productos = controllerRestaurante.darMenu();
+        int j = 1;
+        for (ProductoMenu i: productos)
+        {
+        	System.out.println(j + ".) " +i.darNombre() +" $" + i.darPrecio());
+        	j  ++;
+        }
+        
+        System.out.println("\nCombos:\n");
+        List<Combo> combos = controllerRestaurante.darCombos();
+        j = 1;
+        for (Combo i: combos)
+        {
+        	System.out.println(j + ".) " +i.darNombre() +" $" + i.darPrecio());
+        	j  ++;
+        }        
+        
+        // Ingredientes adicionales
+        System.out.println("\nIngredientes adicionales:\n");
+        List<Ingrediente> ingredientes = controllerRestaurante.darIngredientes();
+        j = 1;
+        for (Ingrediente i: ingredientes)
+        {
+        	System.out.println(j + ".) " +i.darNombre() +" $" + i.darCostoAdicional());
+        	j  ++;
+        }   
+
 	}
 	
 	private void ejecutarCargarData()
 	{
 		try
 		{
-			archivo = "./data/atletas.csv";
-			controller = LoaderRestaurante.cargarArchivo(archivo);
-			System.out.println("Se cargó el archivo " + archivo + " con información de los Juegos Olímpicos.");
-			Collection<String> eventos = calculadora.darNombresDeportes();
-			System.out.println("Los deportes para los que se tiene información son:");
-			for (String dep : eventos)
-			{
-				System.out.println(" - " + dep);
-			}
+			String archivoCombos = "./data/combos.txt";
+			String archivoIngredientes = "./data/ingredientes.txt";
+			String archivoMenu = "./data/menu.txt";
+			//archivo = "./data/atletas.csv";
+			//System.out.println(archivoCombos + archivoIngredientes + archivoMenu);
+			controllerRestaurante = LoaderRestaurante.cargarArchivo(archivoMenu,archivoIngredientes,archivoCombos);
+			System.out.println("Se cargó el archivo " + archivoCombos + " con información de los combos.");
+			System.out.println("Se cargó el archivo " + archivoIngredientes + " con información de los ingredientes.");
+			System.out.println("Se cargó el archivo " + archivoMenu + " con información del menu.");
+
 		}
 		catch (FileNotFoundException e)
 		{
-			System.out.println("ERROR: el archivo indicado no se encontró.");
+			System.out.println("ERROR: el archivo indicado no se encontro.");
 		}
 		catch (IOException e)
 		{
@@ -128,10 +174,35 @@ public class Consola {
 
 	}
 	
+	/**
+	 * Este método sirve para imprimir un mensaje en la consola pidiéndole
+	 * información al usuario y luego leer lo que escriba el usuario.
+	 * 
+	 * @param mensaje El mensaje que se le mostrará al usuario
+	 * @return La cadena de caracteres que el usuario escriba como respuesta.
+	 */
+	public String input(String mensaje)
+	{
+		try
+		{
+			System.out.print(mensaje + ": ");
+			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+			return reader.readLine();
+		}
+		catch (IOException e)
+		{
+			System.out.println("Error leyendo de la consola");
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	
 	public static void main(String[] args)
 	{
 		Consola consola = new Consola();
-		consola.ejecutarAplicacion();
 		consola.ejecutarCargarData();
+		consola.ejecutarAplicacion();
+		
 	}
 }
